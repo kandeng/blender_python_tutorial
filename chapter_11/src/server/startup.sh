508dc51a87ec4af47dea06cc1eb91b6af5de0dfc
Kan Deng <kan.deng@qq.com>
2025-12-18 16:21:53 +0800

    Add the startup and shutdown of the rabbitmq
    Add the startup and shutdown of the rabbitmq

--- chapter_11/src/server/startup.sh
+++ chapter_11/src/server/startup.sh
@@ -7,4 +7,20 @@ export PYTHONPATH
 
 mkdir -p logs
 
-nohup python3 langchain/blender_agent_server.py > logs/blender_agent_server_log.txt 2>&1 &
+echo
+echo "[INFO] Stop rabbitmq-server to clean up the environment. Then wait for 5 seconds ..."
+sudo systemctl stop rabbitmq-server
+sleep 5
+
+echo
+echo "[INFO] Start the rabbitmq-server. Then wait for another 5 seconds ..."
+sudo systemctl start rabbitmq-server
+sleep 5
+
+echo
+echo "[INFO] nohup python3 langchain/orchestrator_agent.py > logs/orchestrator_agent_log.txt"
+nohup python3 langchain/orchestrator_agent.py > logs/orchestrator_agent_log.txt 2>&1 &
+
+echo
+echo "[INFO] nohup python3 fastapi_server/fastapi_engine.py > logs/fastapi_engine_log.txt"
+nohup python3 fastapi_server/fastapi_engine.py > logs/fastapi_engine_log.txt 2>&1 &
