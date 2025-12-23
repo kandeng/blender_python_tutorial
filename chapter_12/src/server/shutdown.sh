dae87bb82ec24ad09e7f64abaf874ef29edbc71f
Kan Deng <kan.deng@qq.com>
2025-12-23 20:22:30 +0800

    Add the commands to startup and shutdown chroma
    Add the commands to startup and shutdown chroma vector database.

--- chapter_12/src/server/shutdown.sh
+++ chapter_12/src/server/shutdown.sh
@@ -1,21 +1,40 @@
 #!/bin/bash   #isn't a simple comment 
 echo 
 echo "[INFO] shutdown.sh is shuting down the Blender AI agent system."
-
-echo
-echo "[INFO] Stop rabbitmq-server to clean up the environment. Then wait for 5 seconds ..."
-sudo systemctl stop rabbitmq-server
-sleep 5
+echo 
 
 PYTHONPATH="${PYTHONPATH}:${PWD}"
 export PYTHONPATH 
 
+
+echo 
+echo 
+echo "=========================================================================================="
+echo "    [1] Shut down chroma vector database, then wait for 5 seconds."
+
+cd /home/robot/aiBlender/aiBlender_20251218/server
+CHROMA_PID=$(cat ./logs/chroma.pid)
+kill $CHROMA_PID
+sleep 5
+
+# Verify shutdown (curl will fail)
+curl http://localhost:5566/api/v2/heartbeat 2>&1
+echo "[INFO] Expect to see the error message: 'Failed to connect to ...'"
+echo 
+
+# Clean up pid file (optional)
+rm ./logs/chroma.pid 
+
+
+
+echo 
+echo 
+echo "=========================================================================================="
+echo "    [2] Shut down fastapi web server, then wait for 5 seconds."
+
 # Reference: Is there a way to kill uvicorn cleanly?
 # https://stackoverflow.com/questions/60424390/is-there-a-way-to-kill-uvicorn-cleanly
 SERVER_PID="$(pgrep -f 'fastapi_engine')"
-
-echo
-echo "[INFO] Terminate fastapi_engine: " 
 echo $(ps -p $SERVER_PID -f -o pid -o command | tail -n +2)
 
 if [[ -n "$SERVER_PID" ]]
@@ -23,16 +42,27 @@ then
     # PGID="$(ps --no-headers -p $PID -o pgid)"
     PGID="$(ps -p $SERVER_PID -o pgid | tail -n +2)"
     echo "    PGID: $PGID, PID: $SERVER_PID"
-    echo 
+    
     # kill -SIGINT -- -${PGID// /}
     kill -SIGKILL -- ${SERVER_PID// /}
 fi
 
+sleep 5
+
+echo 
+echo 
+echo "=========================================================================================="
+echo "    [3] Shut down rabbitmq message queue service, then wait for 5 seconds."
+sudo systemctl stop rabbitmq-server
+sleep 5
+
+
+echo 
+echo 
+echo "=========================================================================================="
+echo "    [4] Shut down langchain agents"
 
 AGENT_PID="$(pgrep -f 'orchestrator_agent')"
-
-echo
-echo "[INFO] Terminate orchestrator_agent: " 
 echo $(ps -p $AGENT_PID -f -o pid -o command | tail -n +2)
 
 if [[ -n "$AGENT_PID" ]]
@@ -44,3 +74,10 @@ then
     # kill -SIGINT -- -${PGID// /}
     kill -SIGKILL -- ${AGENT_PID// /}
 fi
+
+echo 
+echo 
+echo "=========================================================================================="
+echo 
+echo 
+echo "[INFO] shutdown.sh is completed."
--- chapter_12/src/server/startup.sh
+++ chapter_12/src/server/startup.sh
@@ -1,26 +1,66 @@
 #!/usr/bin/bash  #isn't a simple comment 
 echo
 echo "[INFO] startup.sh is starting up the Blender AI Agent server."
+echo 
 
 PYTHONPATH="${PYTHONPATH}:${PWD}"
 export PYTHONPATH 
 
 mkdir -p logs
 
-echo
-echo "[INFO] Stop rabbitmq-server to clean up the environment. Then wait for 5 seconds ..."
-sudo systemctl stop rabbitmq-server
+echo 
+echo 
+echo "=========================================================================================="
+echo "    [1] Start up chroma vector database, then wait for 5 seconds."
+
+cd /home/robot/aiBlender/aiBlender_20251218/server
+nohup chroma run --host 127.0.0.1 --port 5566 --path ~/chroma_storage > ./logs/chroma.log 2>&1 &
 sleep 5
 
+# Get the Chroma process ID (PID) (to manage later)
+echo $! > ./logs/chroma.pid
+  
+# Check if port 5566 is in use (Linux/macOS)
 echo
-echo "[INFO] Start the rabbitmq-server. Then wait for another 5 seconds ..."
+sudo lsof -i :5566 2>&1
+
+# Test connectivity (should return a heartbeat timestamp)
+echo
+curl http://localhost:5566/api/v2/heartbeat 2>&1
+
+
+echo 
+echo 
+echo "=========================================================================================="
+echo "    [2] Start up fastapi web server, then wait for 5 seconds."
+echo
+echo " nohup python3 fastapi_server/fastapi_engine.py > logs/fastapi_engine_log.txt"
+cd /home/robot/aiBlender/aiBlender_20251218/server
+nohup python3 fastapi_server/fastapi_engine.py > logs/fastapi_engine_log.txt 2>&1 &
+sleep 5
+
+echo 
+echo 
+echo "=========================================================================================="
+echo "    [3] Start up rabbitmq message queue service, then wait for 5 seconds."
+echo 
 sudo systemctl start rabbitmq-server
 sleep 5
 
+
+echo 
+echo 
+echo "=========================================================================================="
+echo "    [4] Shut down langchain agents"
 echo
-echo "[INFO] nohup python3 langchain_agent/orchestrator_agent.py > logs/orchestrator_agent_log.txt"
+echo " nohup python3 langchain_agent/orchestrator_agent.py > logs/orchestrator_agent_log.txt"
+cd /home/robot/aiBlender/aiBlender_20251218/server
 nohup python3 langchain_agent/orchestrator_agent.py > logs/orchestrator_agent_log.txt 2>&1 &
 
-echo
-echo "[INFO] nohup python3 fastapi_server/fastapi_engine.py > logs/fastapi_engine_log.txt"
-nohup python3 fastapi_server/fastapi_engine.py > logs/fastapi_engine_log.txt 2>&1 &
+
+echo 
+echo 
+echo "=========================================================================================="
+echo 
+echo 
+echo "[INFO] startup.sh is completed."
