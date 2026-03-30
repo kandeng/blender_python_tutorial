75a8e39cbd1787f743ded795afee3c72adb22d6d
Kan Deng <kan.deng@qq.com>
2026-03-30 21:28:51 +0800

    Update index.ts
    Update index.ts, add the test of api.config and api.pluginConfig

--- chapter_17/src/plugins/hello-tool-plugin/src/index.ts
+++ chapter_17/src/plugins/hello-tool-plugin/src/index.ts
@@ -1,7 +1,6 @@
 // index.ts
 import { definePluginEntry } from "openclaw/plugin-sdk/plugin-entry";
 import { Type } from "@sinclair/typebox";
-
 import type { OpenclawPluginApi } from "./plugin-api";
 import { registerService } from "./service";
 
@@ -11,14 +10,28 @@ export default definePluginEntry({
     description: "A demo custom tool plugin of openclaw, to be called on intention.",
 
     register(api) {
-        const { logger } = api;
+        const { logger, config, pluginConfig } = api;
 
-        logger.info("**************************************************");
-        logger.info("🚀 SUCCESS: [hello-tool-plugin] active for 邓侃 (Kan Deng)!");
-        logger.info("Timestamp: 2026.03.28, 18:00");
-        logger.info("**************************************************");
+        // console.log("Following is the content of api.config from 'openclaw.json': ")
+        // console.log(JSON.stringify(config, null, 2)); 
 
-        // Register the rosbridge WebSocket connection as a managed service
+        // console.log("Following is the content of api.config from 'openclaw.plugin.json': ")
+        // console.log(JSON.stringify(pluginConfig, null, 2));  
+
+        logger.info("\n**************************************************");
+        logger.info("🚀 [hello-tool-plugin] the content of config.plugins.load");
+        const plugin_load_str = JSON.stringify(config.plugins.load, null, 2)
+        logger.info(` "openclaw.json" config.plugins.load: '${plugin_load_str}' `);
+        logger.info("\n");
+        
+        logger.info("\n**************************************************");
+        logger.info("⚠️ [hello-tool-plugin] the content of pluginConfig.nodeUrl");
+        const plugin_nodeurl_str = JSON.stringify(pluginConfig.nodeUrl, null, 2)
+        logger.info(` "openclaw.plugin.json" pluginConfig.nodeUrl: '${plugin_nodeurl_str}' `);
+        logger.info("\n");
+        
+ 
+        // Register the rosbridge WebSocket connection as a manageind service
         registerService(api);
 
         logger.info("[hello-tool-plugin] registerService loaded successfully");
